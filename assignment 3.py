e771eca80b5396734a8fd66a8755a6f03dcbc79d
= <=>
2018-06-16 01:25:11 +0530

    assignment four

---
+++ assignment 3.py
@@ -0,0 +1,4 @@
+l=[2,5,8]
+tupple(l)
+len(l)
+print(tupple(l)
---
+++ assignment 4.py
@@ -0,0 +1,56 @@
+#1
+
+'''l=(2,5,8)
+print(len(l))'''
+
+#2
+'''a=('apple')
+print(max(a))
+print(min(a))'''
+
+#3
+'''x=(1,2,3,4)
+y=1*2*3*4
+print(y)'''
+
+
+#4 difference of sets
+'''a=set([1,2,3])
+b=set([3,4,5])
+a-b
+print(a-b)
+b-a
+print(b-a)'''
+
+#compare two sets
+'''a=set([1,2,3])
+b=set([3,4,5])
+print(a<=b)'''
+
+#intersection of two sets
+'''a=set([1,2,3])
+b=set([3,4,5])
+print(a&b)'''
+
+#5 dictionary
+'''a=input("enter name")
+b=input("enter marks")
+c=input("enter name")
+d=input("enter marks")
+e=input("enter name")
+f=input("enter marks")
+g=input("enter name")
+h=input("enter marks")
+i=input("enter name")
+j=input("enter marks")
+D={a:b,c:d,e:f,g:h,i:j}
+print(D)'''
+
+#6
+'''l=["M","I","S","S","I","S","S","I","P","P","I"]
+m=l.count("M")
+i=l.count("I")
+s=l.count("S")
+p=l.count("P")
+D={'m':m,'i':i,'s':s,'p':p}
+print(D)'''
